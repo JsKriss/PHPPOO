eed9c3ea9f56e3f23cb19aafc29799b9fe78e0f4
JsKriss <57392242+JsKriss@users.noreply.github.com>
2020-01-30 13:03:57 +0100

    formulaire ajout reset submit

--- index.php
+++ index.php
@@ -5,29 +5,10 @@ spl_autoload_register('classAutoLoader');
 
 $formulaire = new Form('index.php?page=validation','FrmContact');
 
-
+$html = $formulaire->beginHtml( 'je fait des formulaires en objet');
 echo $formulaire->displayForm();
-
-$chat = new Mammifere();
-var_dump($chat);
-
-
-
-
-$toto = new Coupe('Peugeot','504','vert',[2000,1000,1000],1200);
-var_dump($toto);
-
-$voiture1 = new Voiture( 'lada','Niva','Blanc', [2000,1500,800], 1000  );
-$voiture2 = new Voiture('Nissan','Cube','violet',[2500, 1800, 1200],1200);
-
-
-
-$voiture1->vitesse = 25;
-$voiture2->vitesse = 30;
-
-echo $voiture1->calculerEnergieCinetique().'<br>';
-$voiture1->vitesse= 20;
-echo $voiture1->calculerEnergieCinetique();
+$html .= $formulaire->endHtml();
+echo $html;
 
 
 
